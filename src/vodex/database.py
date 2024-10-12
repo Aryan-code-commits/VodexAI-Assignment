70b907de8d393a9963b5d5afa294b974ff20524f
Aryan Gupta <aryangupta7057@gmail.com>
2024-10-12 23:50:52 +0530

    Updated MongoDB connection string and added environment variable support

--- src/vodex/database.py
+++ src/vodex/database.py
@@ -1,8 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import ConnectionFailure
+import os
 
 # Use the local MongoDB connection string
-MONGO_DETAILS = "mongodb://localhost:27017"  # Local connection string
+MONGO_DETAILS = os.getenv("MONGO_DETAILS")
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
 
